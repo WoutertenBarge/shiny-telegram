4e18daf8a48c87af7d091980e3db1a59287ff92e
Wouter ten Barge <wtbarge@gmail.com>
2019-10-03 12:24:30 +0200

    Mijn eerste commit

---
+++ index.js
@@ -0,0 +1,37 @@
+/*- 
+
+Als gebruiker wil ik een initieel bericht zien in de console die de gebruiker begroet 
+en vraagt om zijn of haar naam.  "Welkom! Wat is je naam?"
+Hint: gebruik "console.log()" voor je berichten.
+*/
+
+
+
+
+
+
+
+
+
+//- Als gebruiker wil ik mijn naam kunnen invoeren in de console en op enter drukken.
+
+//- Als gebruiker wil ik nadat ik mijn naam heb ingevoerd, een bericht terug zien: "Hey [naam]"
+
+/*- Als spelmaker wil ik dat de gebruiker wel een reeële kans heeft om te winnen. 
+Daarom pak ik een willekeurig getal van 0 tot 25.
+Hint: gebruikt [Math.random()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)*/
+
+/*ebruiker wil ik na de begroeting met mijn naam gevraagd worden om een nieuwe input 
+met een cijfer. e.g: "Voer een nummer in van 0 tot 25 om te beginnen met raden..." */
+
+// Als gebruiker wil ik een nummer in kunnen voeren.
+
+
+/*- Als gebruiker wil ik een negatief bericht ontvangen als ik het nummer verkeerd 
+heb geraden e.g. "Dat is niet correct" . Daarna wil ik gevraagd worden om opnieuw te raden.*/
+
+/*- Als gebruiker wil ik een positief bericht ontvangen als ik het nummer goed heb geraden. 
+e.g: "Gefeliciteerd je hebt gewonnen". Het spel is nu af.*/
+
+/*- Als gebruiker wil ik een bericht zien als het spel af is.
+ "e.g: Dag [naam]. Tot de volgende keer"*/
